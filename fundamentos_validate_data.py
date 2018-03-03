addc660b1e96eb53fa58c059c5d810da2e100f23
eslozano <eslozano@espol.edu.ec>
2018-03-02 20:04:51 -0500

    First version validation script

---
+++ fundamentos_validate_data.py
@@ -0,0 +1,102 @@
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+from estudiante import Estudiante
+import pandas as pd
+
+OPCION_NO_VALIDA = 'dato_no_valido'
+VACIO = 'dato_vacio'
+FUERA_DE_RANGO = 'dato_fuera_de_rango'
+NO_REDONDEADO = 'dato_no_redondeado'
+TIPO_NO_DECIMAL = 'dato_no_decimal'
+
+
+
+def fillCell(cell,color,fill_type=None):
+	fill = PatternFill(fgColor=color,fill_type=fill_type)
+	cell.fill = fill
+
+def validateDataSheet(filename,columnas,colores,errorsMessages,validaciones):
+	header_rows = 2
+	try:
+		wb = load_workbook(filename)
+		data_sheet = wb["DATA"]
+		total_students = data_sheet.max_row - header_rows
+		total_columns = data_sheet.max_column 
+
+		print("Total estudiantes a revisar: "+str(total_students))
+		total_errors = 0
+		for row in data_sheet.iter_rows(min_row=header_rows+1, max_col=total_columns, max_row=data_sheet.max_row):
+			data = []
+			for cell in row:
+				data.append(cell.value)
+				fillCell(cell,colores[errorsMessages[0]]) #Restore cell color
+			estudiante = Estudiante(data)
+			errores = estudiante.validar(errorsMessages,validaciones)
+			countErrorStudent = 0
+			for error, campos in errores.items():
+				color = colores[error]
+				countErrorStudent += len(campos)
+				for c in campos:
+					if c in columnas:
+						i = columnas.index(c)
+						fillCell(row[i],color,'solid')
+			if countErrorStudent:
+				print('{:<9}{}'.format('FALLÓ',estudiante.nombre))
+				total_errors += 1
+			else:
+				print('{:<9}{}'.format('OK',estudiante.nombre))
+		print("RESULTADOS: Se detectaron errores en las calificaciones de ", total_errors," estudiantes.")
+		print("Revise en el archivo las celdas resaltadas de colores:")
+		print("Naranja(vacio), Azul(opcion_no_valida), Amarillo(fuera_de_rango) o Verde(necesita_mejoamiento).")
+		wb.save(filename)
+	except IOError as e:
+		print("ERROR: No." , e.errno , e )
+		if e.errno == 2:
+			print("El archivo ",filename," no se encuentra en la carpeta del script.")
+		if e.errno == 13:
+			print("El archivo ",filename," se encuentra abierto.")
+
+def dataAnalysis(filename):
+	df = pd.read_excel("filename",skiprows =2)
+
+
+
+#Escriba el nombre del archivo de datos
+filename = "PAR7_2017II.xlsx"
+#Las columnas deben estar en el orden del excel
+columnas = ["nombre","matricula","genero","paralelo","cod_carrera","veces_tomadas",
+			"1er_proyecto", "1er_sustent", "1er_calif_final",
+			"1er_exam_tema1","1er_exam_tema2","1er_exam_tema3","1er_exam_tema4",
+			"2do_proyecto", "2do_sustent", "2do_calif_final",
+			"2do_exam_tema1","2do_exam_tema2","2do_exam_tema3",
+			"calif_final_practica","AP_sin_mejoramiento",
+			"3er_proyecto","3er_calif_final",
+			"3er_exam_tema1","3er_exam_tema2","3er_exam_tema3"
+]
+#Los colores en los que se resaltarán las celdas según los errores
+colors = {
+	VACIO:"ffd8bf", #naranja 
+	OPCION_NO_VALIDA: '6aa2fc', #azul 
+	FUERA_DE_RANGO: 'fffa00', #amarillo
+	NO_REDONDEADO: '7cff94', #verde
+	TIPO_NO_DECIMAL: 'ff7ff4' #rosado
+}
+
+
+
+errorsMessages= [VACIO,OPCION_NO_VALIDA,FUERA_DE_RANGO,NO_REDONDEADO,TIPO_NO_DECIMAL]
+
+validaciones = {	
+	"genero": ["F","M"],
+	"veces_tomadas":[2,3],
+	"calif_final": 100,
+	"sustent": 1,
+	"proyecto": {"1er_proyecto":20,"2do_proyecto":20,"3er_proyecto":25},
+	"examen":{
+		"1er_exam_": { "tema1": 20, "tema2": 30, "tema3": 45, "tema4": 10 },
+		"2do_exam_": { "tema1": 35, "tema2": 55, "tema3": 10 },
+		"3er_exam_": { "tema1": 40, "tema2": 55, "tema3": 10 } #el tema 2 tiene bono de 5 puntos
+	}
+}
+
+validateDataSheet(filename,columnas,colors,errorsMessages, validaciones)
